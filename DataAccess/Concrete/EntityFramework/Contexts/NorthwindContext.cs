7af469a3464be9bf295cfdf47bd9e5eb6bdd397d
said baradai <saitbaradai@gmail.com>
2022-07-08 23:49:24 +0300

    Added: Entity framwork

---
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    internal class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: "server=DESKTOP-7K06ESJ;dtabase=;Integrated Security=True");
+        }
+    }
+}
