13e2cd34f87634f16282ec496bb45d0646b583f1
said baradai <saitbaradai@gmail.com>
2022-07-11 19:33:33 +0300

    added : result and dataresul under Core\utility

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public bool IsSuccess { get; }
+
+        public string Message { get; }
+
+        public Result(bool isSuccess)
+        {
+            isSuccess = IsSuccess;
+        }
+
+        public Result(bool isSuccess, string message) : this(isSuccess)
+        {
+            Message = message;
+        }
+    }
+}
