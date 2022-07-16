a150266254fb96771609c4eccb8a899edf427990
said baradai <saitbaradai@gmail.com>
2022-07-17 02:25:49 +0300

    add auth class to business layer

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,6 +10,14 @@ namespace Business.Constants
         public static string ProductAdded   = "Product has been ADDED successfuly";
         public static string ProductUpdated = "Product has been UPDATED successfuly";
         public static string ProductDeleted = "Product has been DELETED successfuly";
+
+        public static string UserNotFound   = "User not found";
+
+        public static string PasswordIncorrect = "Password is incorrect";
+        public static string SuccessfulLogin = "Successful login";
+        public static string UserAlreadyExists = "User already exists";
+        public static string UserAddSuccessfuly = "User add successfuly";
+        public static string AccessTokenCreated = "Access token created";
     }
 }
 
